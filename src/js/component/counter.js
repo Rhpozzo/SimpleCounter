ab9c85173c863af6025578705ec17c3c8a648967
Román Pozzo <rhpozzo@gmail.com>
2019-11-19 23:21:49 +0000

    first commit

---
+++ src/js/component/counter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Counter = props => {
+	return (
+		<div className="container">
+			<div className="main-counter">
+				<div className="firstDigit">{props.sthDigit} - </div>
+				<div className="firstDigit">{props.fifDigit} - </div>
+				<div className="firstDigit">{props.fthDigit} - </div>
+				<div className="firstDigit">{props.trdDigit} - </div>
+				<div className="firstDigit">{props.sndDigit} - </div>
+				<div className="firstDigit">{props.fstDigit}</div>
+			</div>
+		</div>
+	);
+};
+Counter.propTypes = {
+	fstDigit: PropTypes.number,
+	sndDigit: PropTypes.number,
+	trdDigit: PropTypes.number,
+	fthDigit: PropTypes.number,
+	fifDigit: PropTypes.number,
+	sthDigit: PropTypes.number
+};
+export default Counter;
--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,30 @@ import "../styles/index.scss";
 
 //import your own components
 import { Home } from "./component/home.js";
+import { Counter } from "./component/counter.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let count = 0;
+
+setInterval(() => {
+	let a = Math.floor(count / 1) % 10;
+	let b = Math.floor(count / 10) % 10;
+	let c = Math.floor(count / 100) % 10;
+	let d = Math.floor(count / 1000) % 10;
+	let e = Math.floor(count / 10000) % 10;
+	let f = Math.floor(count / 100000) % 10;
+	count++;
+
+	ReactDOM.render(
+		<Counter
+			fstDigit={a}
+			sndDigit={b}
+			trdDigit={c}
+			fthDigit={d}
+			fifDigit={e}
+			sthDigit={f}
+		/>,
+		document.querySelector("#app")
+	);
+}, 10);
+ReactDOM.render(<Counter />, document.querySelector("#app"));
